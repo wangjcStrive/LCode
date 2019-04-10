705a0f6936499200d3bbf5880a69b35246b35397
jicwang <wangjc857@gmail.com>
2019-04-10 16:56:11 +0800

    LCode 414. Third Maximum Number

--- LCode/solutions.h
+++ LCode/solutions.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <set>
+
 
 using namespace std;
 
 struct ListNode {
 	int val;
-	ListNode *next;
+	ListNode* next;
 	ListNode(int x) : val(x), next(NULL) {}
 };
 
@@ -31,13 +33,13 @@ public:
 	// boundary conditions
 	// 1. input [1] 1 -> output should be [1]
 	// 2. n > link list length
-	// ¿ÉÒÔÔÙheadÇ°Ãæ¼ÓÒ»¸ödummyµÄÍ·!!
+	// å¯ä»¥å†headå‰é¢åŠ ä¸€ä¸ªdummyçš„å¤´!!
 	ListNode* removeNthFromEnd(ListNode* head, int n) {
 		if (head == nullptr)
 			return head;
-		ListNode* pFast = head;
-		ListNode* pSlow = head;
-		ListNode* pPre = head;
+		ListNode * pFast = head;
+		ListNode * pSlow = head;
+		ListNode * pPre = head;
 		while (n > 1 && head != nullptr)
 		{
 			pFast = pFast->next;
@@ -57,14 +59,14 @@ public:
 
 
 	// 141. Linked List Cycle
-	bool hasCycle(ListNode *head) {
+	bool hasCycle(ListNode* head) {
 		if (head == NULL)
 			return false;
 
-		ListNode* fast = head->next;
-		ListNode* slow = head;
-		
-		//ÕâÖÖÐ´·¨²»ºÃ¿´
+		ListNode * fast = head->next;
+		ListNode * slow = head;
+
+		//è¿™ç§å†™æ³•ä¸å¥½çœ‹
 /*
 		while (fast!=NULL && fast->next != NULL)
 		{
@@ -87,9 +89,67 @@ public:
 	}
 
 	// 148. Sort List
-	ListNode* sortList(ListNode& head)
+	ListNode* sortList(ListNode & head)
 	{
 
+	}
+
+	//414. Third Maximum Number
+	int thirdMax(vector<int> & nums) {
+		int result = 0;
+		switch (nums.size())
+		{
+		case 0:
+			result = 0;
+			break;
+		case 1:
+			result = nums[0];
+			break;
+		case 2:
+			result = nums[0] > nums[1] ? nums[0] : nums[1];
+			break;
+		default:
+		{
+			vector<int> maxThirdArray;
+			maxThirdArray.push_back(nums[0]);
+			if (nums[1] > nums[0])
+				maxThirdArray.insert(maxThirdArray.begin(), nums[1]);
+			else
+				maxThirdArray.push_back(nums[1]);
+			for (size_t index = 2; index < nums.size(); index++)
+			{
+				for (int i = 0; i < 3; i++)
+				{
+					if (nums[index] > maxThirdArray[i])
+						maxThirdArray.insert(maxThirdArray.begin() + i, nums[index]);
+				}
+			}
+			result = maxThirdArray[0];
+		}
+		break;
+		}
+		return result;
+	}
+
+	int thirdMax_1(vector<int> & nums)
+	{
+		set<int> top3;
+		for (int num : nums)
+		{
+			top3.insert(num);
+			if (top3.size() > 3)
+				top3.erase(top3.begin());
+		}
+		return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
+	}
+
+	int thirdMax_2(vector<int> & nums)
+	{
+		set<int> top3;
+		for (int num : nums)
+			if (top3.insert(num).second && top3.size() > 3)
+				top3.erase(top3.begin());
+		return top3.size() == 3 ? *top3.begin() : *top3.rbegin();
 	}
 };
 
